5746c2eca366096fa54cf01e6647a55dc1d28988
sbobkov <serj.bobkov@gmail.com>
2017-08-30 12:52:41 +0300

    swagger annotations were added

--- src/main/java/ru/bsa/tinkoff/web/ExerciseRestService.java
+++ src/main/java/ru/bsa/tinkoff/web/ExerciseRestService.java
@@ -1,5 +1,9 @@
 package ru.bsa.tinkoff.web;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import ru.bsa.tinkoff.services.application.exception.ApplicationNotFoundException;
 import ru.bsa.tinkoff.services.contact.exception.ContactNotFoundException;
 import ru.bsa.tinkoff.web.dto.ApplicationResponse;
@@ -14,7 +18,15 @@ public interface ExerciseRestService {
     @GET
     @Path("/application/last/{contactId}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    ApplicationResponse getLastApplication(@PathParam("contactId") int contactId) throws ApplicationNotFoundException, ContactNotFoundException;
+    @ApiOperation(
+            value = "Get last application for contact",
+            response = ApplicationResponse.class
+    )
+
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Application not found for contact with target id or contact with target id not found")
+    })
+    ApplicationResponse getLastApplication(@ApiParam(value = "Target contact id", required = true) @PathParam("contactId") int contactId) throws ApplicationNotFoundException, ContactNotFoundException;
 
 
 }
